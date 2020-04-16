e3fb90a79b1efbc2bad3a787c824601006ef2d36
booldook <booldook@gmail.com>
2020-04-16 14:59:54 +0900

    완성!

--- src/views/Weekly.vue
+++ src/views/Weekly.vue
@@ -1,8 +1,9 @@
 <template lang="pug">
 	ul.list-wrap
 		li.list.d-flex.justify-content-between.align-items-center.mb-1(v-for="v in GET_WEEKLY.list" :key="v.dt")
-			img.mx-3(:src="v.weather[0].icon")
-			div
+			div.icon.text-center
+				img(:src="v.weather[0].icon")
+			div.cont
 				h5 {{ v.main.temp }} 도
 				div
 					span.mr-2 {{ v.weather[0].main }}
@@ -25,6 +26,7 @@ export default {
 <style scoped lang="less">
 	.list-wrap {overflow-y: auto;}
 	.list {border: 1px solid #eee;}
-	.list > div {flex: 80% 0 1;}
-	.list > img {max-width: 40px;}
+	.list > .icon {flex: 13% 0 1;}
+	.list > .cont {flex: 85% 0 1;}
+	.list img {max-width: 40px;}
 </style>
